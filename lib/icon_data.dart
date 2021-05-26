2c413a0daad4c8f00ba2e1d8111bf3786a6006a0
Chorn Thorn <chornthorn.kh@gmail.com>
2021-05-27 05:47:33 +0700

    first commit

---
+++ lib/icon_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class TablerIconData extends IconData {
+  const TablerIconData(int codePoint)
+      : super(
+          codePoint,
+          fontFamily: 'TablerIcons',
+          fontPackage: 'tabler_icons',
+        );
+}
